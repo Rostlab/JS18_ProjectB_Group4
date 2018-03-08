d3afe32d5a9f3decafd93fac87dcf64afcea483c
Peeranut Chindanonda <pcxd@me.com>
2018-03-08 21:25:22 +0100

    Fix wrong CORS port and change start message

--- app.js
+++ app.js
@@ -3,18 +3,21 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const routes = require('./api/routes');
 
+const port = process.env.PORT || 3000;
+
 const app = express();
 app.use(bodyParser.json());
 const corsOptions = {
-  origin: 'http://localhost:4200',
+  origin: `http://localhost:${port}`,
   credentials: true,
   allowedHeaders: ['Content-Type', 'Authorization'],
   methods: ['GET', 'PUT', 'POST', 'DELETE'],
 };
 app.use(cors(corsOptions));
 app.use('/', routes);
-const port = process.env.PORT || 3000;
+
 app.listen(port);
 
-console.log(`try this:\ncurl http://localhost:${port}`);
+console.log(`Server is listening on port ${port}`);
+console.log(` Visit here: http://localhost:${port}`);
 module.exports = app;
