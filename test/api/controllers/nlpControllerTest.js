9055050e048e71be27b412771a2b19a3b21beb92
fhnaseer <fhnaseer@live.com>
2018-03-20 17:33:40 +0100

    Using regular expressions to match rules for general functions,

--- test/api/controllers/nlpControllerTest.js
+++ test/api/controllers/nlpControllerTest.js
@@ -3,6 +3,93 @@ const request = require('supertest');
 const server = require('../../../app');
 
 describe('api.controllers.nlpController', () => {
+  it('should return an updateStyle action on POST /api/nlp for sentence "Change plot title to Pie Chart"', (done) => {
+    request(server)
+      .post('/api/nlp')
+      .send({
+        sentence: 'Change plot title to Pie Chart',
+        data: [
+          {
+            type: 'pie',
+          },
+        ],
+        layout: {},
+      })
+      .set('Accept', 'application/json')
+      .expect('Content-Type', 'application/json; charset=utf-8')
+      .expect(200)
+      .expect([
+        {
+          action: 'updateLayout',
+          value: {
+            title: 'Pie Chart',
+          },
+        },
+      ])
+      .end((err) => {
+        should.not.exist(err);
+        done();
+      });
+  });
+
+  it('should return an updateStyle action on POST /api/nlp for sentence "Hide legend"', (done) => {
+    request(server)
+      .post('/api/nlp')
+      .send({
+        sentence: 'Hide legend',
+        data: [
+          {
+            type: 'pie',
+          },
+        ],
+        layout: {},
+      })
+      .set('Accept', 'application/json')
+      .expect('Content-Type', 'application/json; charset=utf-8')
+      .expect(200)
+      .expect([
+        {
+          action: 'updateLayout',
+          value: {
+            showlegend: false,
+          },
+        },
+      ])
+      .end((err) => {
+        should.not.exist(err);
+        done();
+      });
+  });
+
+  it('should return an updateStyle action on POST /api/nlp for sentence "Hide legend"', (done) => {
+    request(server)
+      .post('/api/nlp')
+      .send({
+        sentence: 'Show legend',
+        data: [
+          {
+            type: 'pie',
+          },
+        ],
+        layout: {},
+      })
+      .set('Accept', 'application/json')
+      .expect('Content-Type', 'application/json; charset=utf-8')
+      .expect(200)
+      .expect([
+        {
+          action: 'updateLayout',
+          value: {
+            showlegend: true,
+          },
+        },
+      ])
+      .end((err) => {
+        should.not.exist(err);
+        done();
+      });
+  });
+
   it('should return an updateStyle action on POST /api/nlp for sentence "Display as percentage"', (done) => {
     request(server)
       .post('/api/nlp')
