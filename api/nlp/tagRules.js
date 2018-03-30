b0add607beb0bb712d110decc531d615e13fbd06
sadeg <sadeg@DESKTOP-CFO1GGM>
2018-03-31 01:11:39 +0200

    add traces to nlp functions

--- api/nlp/tagRules.js
+++ api/nlp/tagRules.js
@@ -5,159 +5,210 @@ const BarChart = require('../functions/barChart');
 const Histogram = require('../functions/histogram');
 
 module.exports = {
-  general: [
-    {
-      match: [['ActionHide', 'ComponentLegend']],
-      actions(data, layout, matchRule, matchTags, nlpSentence) {
-        return General.hideLegend();
+    general: [
+      {
+          match: [['ActionHide', 'ComponentLegend']],
+          actions(data, layout, matchRule, matchTags, nlpSentence, traces) {
+              return General.hideLegend();
+          },
       },
+      {
+          match: [['ActionShow', 'ComponentLegend']],
+          actions(data, layout, matchRule, matchTags, nlpSentence, traces) {
+              return General.showLegend();
+          },
+      },
+      {
+          match: [['ActionHide', 'ComponentAxis', 'ComponentGridLine']],
+          actions(data, layout, matchRule, matchTags, nlpSentence, traces) {
+              return General.hideGridline(matchTags.ComponentAxis.data()[0].normal[0]);
+          },
+      },
+      {
+          match: [['ActionShow', 'ComponentAxis', 'ComponentGridLine']],
+          actions(data, layout, matchRule, matchTags, nlpSentence, traces) {
+              return General.showGridline(matchTags.ComponentAxis.data()[0].normal[0]);
+          },
+      },
+      {
+          match: [['AttributePosition', 'ComponentLegend', 'Value+']],
+          actions(data, layout, matchRule, matchTags, nlpSentence, traces) {
+              const tokens = matchTags['Value+']
+                .data()[0]
+                .text.split(',')
+                .map(label => label.trim());
+              return General.changeLegendPosition(tokens[0], tokens[1]);
+          },
+      },
+      {
+          match: [['AttributeSize', 'ComponentLegend', 'Value']],
+          actions(data, layout, matchRule, matchTags, nlpSentence, traces) {
+              const size = matchTags.Value.values().numbers()[0];
+              return General.changeLegendSize(size);
+          },
+      },
+      {
+          match: [['ComponentAxis', 'AttributeTitle', 'Quotation']],
+          actions(data, layout, matchRule, matchTags, nlpSentence, traces) {
+              return General.changeAxisTitle(
+                matchTags.ComponentAxis.data()[0].normal[0],
+                nlpSentence
+                  .quotations(0)
+                  .out('text')
+                  .trim()
+                  .slice(1, -1),
+              );
+          },
+      },
+      {
+          match: [['ComponentAxis', 'AttributeRange', 'Value']],
+          actions(data, layout, matchRule, matchTags, nlpSentence, traces) {
+              return General.changeAxisRange(
+                matchTags.ComponentAxis.data()[0].normal[0],
+                matchTags.Value.values().numbers()[0],
+                matchTags.Value.values().numbers()[1],
+              );
+          },
+      },
+      {
+          match: [['AttributeTitle', 'Quotation']],
+          actions(data, layout, matchRule, matchTags, nlpSentence, traces) {
+              return General.changeTitle(nlpSentence
+                .quotations(0)
+                .out('text')
+                .trim()
+                .slice(1, -1));
+          },
+      },
+    ],
+    bar: [{
+        match: [['ComponentBar', 'AttributeOrder', 'Quotation+']],
+        actions(data, layout, matchRule, matchTags, nlpSentence, traces) {
+            return BarChart.changeCategoryOrder(layout,nlpSentence
+              .quotations().map(quot =>
+              {return quot.out('text')
+              .trim()
+              .slice(1, -1);
+              }));
+        },
+    }
+    ],
+    histogram: [{
+        match: [['ComponentBin', 'Value']],
+        actions(data, layout, matchRule, matchTags, nlpSentence, traces) {
+            return Histogram.setXbins(layout, null, null, null, matchTags.Value.values().numbers()[0]);
+        },
     },
     {
-      match: [['ActionShow', 'ComponentLegend']],
-      actions(data, layout, matchRule, matchTags, nlpSentence) {
-        return General.showLegend();
+        match: [['ComponentAxis', 'AttributeRange','NumericValue+']],
+        actions(data, layout, matchRule, matchTags, nlpSentence, traces) {
+            return Histogram.setXbins(layout, matchTags.NumericValue.values().numbers()[0], matchTags.NumericValue.values().numbers()[1], null, null);
+        },
+    }],
+    pie: [
+      {
+          match: [['ValuePercent']],
+          actions(data, layout, matchRule, matchTags, nlpSentence, traces) {
+              return PieChart.showPercentageValues();
+          },
       },
-    },
-    {
-      match: [['AttributePosition', 'ComponentLegend', 'Value+']],
-      actions(data, layout, matchRule, matchTags, nlpSentence) {
-        const tokens = matchTags['Value+']
-          .data()[0]
-          .text.split(',')
-          .map(label => label.trim());
-        return General.changeLegendPosition(tokens[0], tokens[1]);
+      {
+          match: [['ValueValue']],
+          actions(data, layout, matchRule, matchTags, nlpSentence, traces) {
+              return PieChart.showAbsoluteValues();
+          },
       },
-    },
-    {
-      match: [['AttributeSize', 'ComponentLegend', 'Value']],
-      actions(data, layout, matchRule, matchTags, nlpSentence) {
-        const size = matchTags.Value.values().numbers()[0];
-        return General.changeLegendSize(size);
+      {
+          match: [['Label+', 'ValueColor+']],
+          actions(data, layout, matchRule, matchTags, nlpSentence, traces) {
+              return PieChart.updateColors(
+                data,
+                matchTags['Label+']
+                  .data()[0]
+                  .text.split(',')
+                  .map(label => label.trim()),
+                matchTags['ValueColor+'].out('array')[0].split(' '),
+              );
+          },
       },
-    },
-    {
-      match: [['ComponentAxis', 'AttributeTitle', 'Quotation']],
-      actions(data, layout, matchRule, matchTags, nlpSentence) {
-        return General.changeAxisTitle(
-          matchTags.ComponentAxis.data()[0].normal[0],
-          nlpSentence
-            .quotations(0)
-            .out('text')
-            .trim()
-            .slice(1, -1),
-        );
+    ],
+    scatter: [
+      {
+          match: [['ActionIncrease', 'ComponentMarker', 'AttributeSize', 'Percent']],
+          actions(data, layout, matchRule, matchTags, nlpSentence, traces) {
+              return ScatterPlot.changeMarkerSizeBy(
+                data,
+                1 + (matchTags.Percent.values().numbers()[0] / 100),
+                traces,
+              );
+          },
       },
-    },
-    {
-      match: [['AttributeTitle', 'Quotation']],
-      actions(data, layout, matchRule, matchTags, nlpSentence) {
-            return General.changeTitle(nlpSentence
-            .quotations(0)
-            .out('text')
-            .trim()
-          .slice(1, -1));
+      {
+          match: [['ActionDecrease', 'ComponentMarker', 'AttributeSize', 'Percent']],
+          actions(data, layout, matchRule, matchTags, nlpSentence, traces) {
+              return ScatterPlot.changeMarkerSizeBy(
+                data,
+                1 - (matchTags.Percent.values().numbers()[0] / 100),
+                traces,
+              );
+          },
       },
-    },
-  ],
-  bar: [{
-      match: [['ComponentBar', 'AttributeOrder', 'Quotation+']],
-      actions(data, layout, matchRule, matchTags, nlpSentence) {
-          return BarChart.changeCategoryOrder(layout,nlpSentence
-            .quotations().map(quot =>
-            {return quot.out('text')
-            .trim()
-            .slice(1, -1);
-            }));
+      {
+          match: [['ComponentMarker', 'AttributeSize', 'Value']],
+          actions(data, layout, matchRule, matchTags, nlpSentence, traces) {
+              return ScatterPlot.changeMarkerSize(matchTags.Value.values().numbers()[0], traces);
+          },
       },
-  }
-  ],
-  histogram: [{
-      match: [['ComponentBin', 'Value']],
-      actions(data, layout, matchRule, matchTags, nlpSentence) {
-          return Histogram.setXbins(layout, null, null, null, matchTags.Value.values().numbers()[0]);
+      {
+          match: [['ComponentMarker', 'AttributeOpacity', 'Percent']],
+          actions(data, layout, matchRule, matchTags, nlpSentence, traces) {
+              return ScatterPlot.changeMarkerOpacity(
+                matchTags.Percent.values().numbers()[0] / 100,
+                traces,
+              );
+          },
       },
-  },
-  {
-      match: [['ComponentAxis', 'AttributeRange','NumericValue+']],
-      actions(data, layout, matchRule, matchTags, nlpSentence) {
-          return Histogram.setXbins(layout, matchTags.NumericValue.values().numbers()[0], matchTags.NumericValue.values().numbers()[1], null, null);
+      {
+          match: [['ComponentMarker', 'ValueColor']],
+          actions(data, layout, matchRule, matchTags, nlpSentence, traces) {
+              return ScatterPlot.changeMarkerColor(matchTags.ValueColor.out('normal'), traces);
+          },
       },
-  }],
-  pie: [
-    {
-      match: [['ValuePercent']],
-      actions(data, layout, matchRule, matchTags, nlpSentence) {
-        return PieChart.showPercentageValues();
+      {
+          match: [['ComponentLine', 'ValueColor']],
+          actions(data, layout, matchRule, matchTags, nlpSentence, traces) {
+              return ScatterPlot.changeLineColor(matchTags.ValueColor.out('normal'), traces);
+          },
       },
-    },
-    {
-      match: [['ValueValue']],
-      actions(data, layout, matchRule, matchTags, nlpSentence) {
-        return PieChart.showAbsoluteValues();
+      {
+          match: [['ComponentLine', 'AttributeSize', 'Value']],
+          actions(data, layout, matchRule, matchTags, nlpSentence, traces) {
+              return ScatterPlot.changeLineWidth(matchTags.Value.values().numbers()[0], traces);
+          },
       },
-    },
-    {
-      match: [['ValueLabel+', 'ValueColor+']],
-      actions(data, layout, matchRule, matchTags, nlpSentence) {
-        return PieChart.updateColors(
-          data,
-          matchTags['ValueLabel+']
-            .data()[0]
-            .text.split(',')
-            .map(label => label.trim()),
-          matchTags['ValueColor+'].out('array')[0].split(' '),
-        );
+      {
+          match: [['ActionHide', 'ComponentLine']],
+          actions(data, layout, matchRule, matchTags, nlpSentence, traces) {
+              return ScatterPlot.changeScatterMode('markers', traces);
+          },
       },
-    },
-  ],
-  scatter: [
-    {
-      match: [['ComponentMarker', 'AttributeSize', 'Value']],
-      actions(data, layout, matchRule, matchTags, nlpSentence) {
-        return ScatterPlot.changeMarkerSize(matchTags.Value.values().numbers()[0]);
+      {
+          match: [['ActionShow', 'ComponentLine']],
+          actions(data, layout, matchRule, matchTags, nlpSentence, traces) {
+              return ScatterPlot.changeScatterMode('lines+markers', traces);
+          },
       },
-    },
-    {
-      match: [['ComponentMarker', 'AttributeOpacity', 'Percent']],
-      actions(data, layout, matchRule, matchTags, nlpSentence) {
-        return ScatterPlot.changeMarkerOpacity(matchTags.Percent.values().numbers()[0] / 100);
+      {
+          match: [['ValueSymbol']],
+          actions(data, layout, matchRule, matchTags, nlpSentence, traces) {
+              return ScatterPlot.changeMarkerSymbol(matchTags.ValueSymbol.out('normal'), traces);
+          },
       },
-    },
-    {
-      match: [['ComponentLine', 'ValueColor']],
-      actions(data, layout, matchRule, matchTags, nlpSentence) {
-        return ScatterPlot.changeLineColor(matchTags.ValueColor.out('normal'));
+      {
+          match: [['ValueDash']],
+          actions(data, layout, matchRule, matchTags, nlpSentence, traces) {
+              return ScatterPlot.changeLineDash(matchTags.ValueDash.out('normal'), traces);
+          },
       },
-    },
-    {
-      match: [['ComponentLine', 'AttributeSize', 'Value']],
-      actions(data, layout, matchRule, matchTags, nlpSentence) {
-        return ScatterPlot.changeLineWidth(matchTags.Value.values().numbers()[0]);
-      },
-    },
-    {
-      match: [['ActionHide', 'ComponentLine']],
-      actions(data, layout, matchRule, matchTags, nlpSentence) {
-        return ScatterPlot.changeScatterMode('markers');
-      },
-    },
-    {
-      match: [['ActionShow', 'ComponentLine']],
-      actions(data, layout, matchRule, matchTags, nlpSentence) {
-        return ScatterPlot.changeScatterMode('lines+markers');
-      },
-    },
-    {
-      match: [['ValueSymbol']],
-      actions(data, layout, matchRule, matchTags, nlpSentence) {
-        return ScatterPlot.changeMarkerSymbol(matchTags.ValueSymbol.out('normal'));
-      },
-    },
-    {
-      match: [['ValueDash']],
-      actions(data, layout, matchRule, matchTags, nlpSentence) {
-        return ScatterPlot.changeLineDash(matchTags.ValueDash.out('normal'));
-      },
-    },
-  ],
+    ],
 };
