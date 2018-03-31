971fc960c70be64b2a08e03a2efcc76194e5006e
sadeg <sadeg@DESKTOP-CFO1GGM>
2018-03-31 02:15:07 +0200

    lint

--- api/nlp/tagRules.js
+++ api/nlp/tagRules.js
@@ -87,34 +87,35 @@ module.exports = {
       match: [['ComponentBar', 'AttributeOrder', 'Quotation+']],
       actions(data, layout, matchRule, matchTags, nlpSentence, traces) {
         return BarChart.changeCategoryOrder(layout, nlpSentence
-            .quotations().map(quot => quot.out('text').trim().slice(1, -1))
+          .quotations().map(quot => quot.out('text').trim().slice(1, -1)),
         );
       },
-    }
+    },
   ],
   histogram: [
     {
       match: [['ComponentBin', 'Value']],
       actions(data, layout, matchRule, matchTags, nlpSentence, traces) {
         return Histogram.setXbins(layout, 
-            null,
-            null,
-            null,
-            matchTags.Value.values().numbers()[0],
+          null,
+          null,
+          null,
+          matchTags.Value.values().numbers()[0],
         );
       },
     },
     {
       match: [['ComponentAxis', 'AttributeRange', 'NumericValue+']],
       actions(data, layout, matchRule, matchTags, nlpSentence, traces) {
-        return Histogram.setXbins(layout, 
-            matchTags.NumericValue.values().numbers()[0], 
-            matchTags.NumericValue.values().numbers()[1], 
-            null, 
-            null,
+        return Histogram.setXbins(
+          layout, 
+          matchTags.NumericValue.values().numbers()[0], 
+          matchTags.NumericValue.values().numbers()[1], 
+          null, 
+          null,
         );
       },
-    }
+    },
   ],
   pie: [
     {
